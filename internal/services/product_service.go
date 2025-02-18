6b0c5c0333b0c419246779c3efe116653b66c758
spadaritucks <thiago.spadari02@gmail.com>
2025-02-17 21:56:41 -0300

    commit

---
+++ internal/services/product_service.go
@@ -0,0 +1,46 @@
+package services
+
+import (
+	"github.com/spadaritucks/GOAPI-Eccomerce.git/internal/database"
+	"github.com/spadaritucks/GOAPI-Eccomerce.git/internal/entity"
+)
+
+type ProductService struct {
+	ProductDB database.ProductDB
+}
+
+func NewProductService(productDB database.ProductDB) *ProductService {
+	return &ProductService{ProductDB: productDB}
+}
+
+func (ps *ProductService) GetProducts() ([]*entity.Product, error) {
+	products, err := ps.ProductDB.GetProducts()
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (ps *ProductService) GetProduct(id string) (*entity.Product, error) {
+	product, err := ps.ProductDB.GetProduct(id)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (ps *ProductService) GetProductByCategoryId(categoryID string) ([]*entity.Product, error) {
+	products, err := ps.ProductDB.GetProductByCategoryId(categoryID)
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+func (ps *ProductService) CreateProduct(name string, description string, price float64, categoryID string, imageURL string) (*entity.Product, error) {
+	product := entity.NewProduct(name, description, price, categoryID, imageURL)
+	_, err := ps.ProductDB.CreateProduct(product)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
